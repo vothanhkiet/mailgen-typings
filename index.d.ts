fd8e0418051deeb18e71da27154f14eebf9b2345
vothanhkiet <vothanhkiet@gmail.com>
2016-07-25 00:23:35 -0500

    clean up

--- index.d.ts
+++ index.d.ts
@@ -1,49 +1,47 @@
 /**
  * Created by kiettv on 7/24/16.
  */
-declare module "mailgen" {
-    interface Option {
-        theme:string;
-        product:Product
-    }
+export interface Option {
+    theme:string;
+    product:Product
+}
 
-    interface Product {
-        name:string;
-        link:string;
-        logo?:string;
-    }
+export interface Product {
+    name:string;
+    link:string;
+    logo?:string;
+}
 
-    interface Content {
-        body:ContentBody;
-    }
+export interface Content {
+    body:ContentBody;
+}
 
-    interface ContentBody {
-        name:string;
-        intro:string;
-        action?:Action;
-        outro:string;
-    }
+export interface ContentBody {
+    name:string;
+    intro:string;
+    action?:Action;
+    outro:string;
+}
 
-    interface Action {
-        instructions:string;
-        button:Button;
-    }
+export interface Action {
+    instructions:string;
+    button:Button;
+}
 
-    interface Button {
-        color:string;
-        text:string;
-        link:string;
-    }
+export interface Button {
+    color:string;
+    text:string;
+    link:string;
+}
 
-    export class Mailgen {
-        constructor(opts:Option);
+export class Mailgen {
+    constructor(opts:Option);
 
-        public cacheThemes():void;
+    public cacheThemes():void;
 
-        public generate(params:Content):any;
+    public generate(params:Content):any;
 
-        public generatePlaintext(params:Content):any;
+    public generatePlaintext(params:Content):any;
 
-        public parseParams(params:any)
-    }
+    public parseParams(params:any)
 }
