0c63b163c9c096dfd4bde3e8eaaf00a89fff476d
vothanhkiet <vothanhkiet@gmail.com>
2016-07-24 23:47:48 -0500

    add more interface

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,48 @@
  * Created by kiettv on 7/24/16.
  */
 declare module "mailgen" {
-    export class Mailgen{
+    interface Option {
+        theme:string;
+        product:Product
+    }
+
+    interface Product {
+        name:string;
+        link:string;
+        logo?:string;
+    }
+
+    interface Content {
+        body:ContentBody;
+    }
+
+    interface ContentBody {
+        name:string;
+        intro:string;
+        action?:Action;
+        outro:string;
+    }
+
+    interface Action {
+        instructions:string;
+        button:Button;
+    }
+
+    interface Button {
+        color:string;
+        text:string;
+        link:string;
+    }
+
+    export class Mailgen {
+        constructor(opts:Option);
+
         public cacheThemes():void;
-        public generate(params:any):any;
-        public generatePlaintext(params:any):any;
+
+        public generate(params:Content):any;
+
+        public generatePlaintext(params:Content):any;
+
         public parseParams(params:any)
     }
 }
